9db344108cb3031b7965e2cee9a28e8a07cc8a31
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-08-03 16:32:17 +0200

    removed spaces

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
@@ -10,4 +10,4 @@ def perform_operation(num1: float, num2: float, operation: str):
             return "Error: Division by zero"
         return num1 / num2
     else:
-        return "Error: Invalid arithmetic operation"
+        return "Error: Invalid operation"
