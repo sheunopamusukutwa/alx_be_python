c5736c9f402e9140ce88b19718e45eddc494a72c
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-08-10 21:49:45 +0200

    UPDATE 2 - Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,16 +1,15 @@
 class Book:
 
-    def __init__(self, title: str, author: str, year: int) -> None:
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
-        self.year = int(year)
+        self.year = year
 
     def __del__(self):
-
         print(f"Deleting {self.title}")
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
