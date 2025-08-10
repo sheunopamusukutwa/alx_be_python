54aa661a88c928c68d597223a70723e35a2a3343
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-08-10 21:56:47 +0200

    UPDATED - Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,11 +3,17 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -15,20 +21,17 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count  # number of pages
 
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        # Accepts Book, EBook, or PrintBook
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
